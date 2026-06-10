014081e4b69d7a4fa9027f5f8bb4876a671dd99a
Aïcha KOUREICH <aicha.koureich@telecom-sudparis.eu>
2026-06-10 19:53:02 +0200

    Ajout Player and Entity

---
+++ src/core/AttackType.hpp
@@ -0,0 +1,5 @@
+
+enum class AttackType{
+    STRENGTH,
+    ELOQUENCE,
+};
---
+++ src/core/Entities/Player.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "AttackType.hpp"
+#include "Entity.hpp"
+
+class Player : public Entity {
+ private:
+//Manque tout ce qui concerne Weapon et Items et increaseNoseSize()
+  AttackType currentAttack;
+  int baseDamage;
+  int baseDefense;
+  int noseSize;
+  float speed;
+  bool isBlocking;
+
+ public:
+  void takeDamage(const Entity& attacker, AttackType type) override;
+  void attack(const Entity& target, AttackType type) override;
+  AttackType chooseAttackType();
+  AttackType getCurrentAttackType();
+  Player(int hp);
+  ~Player() override = default;
+};
