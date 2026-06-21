4213097f6ef2677b8beae555639332f1c93bd097
Alexandre <alexandre.gm@outlook.com>
2026-06-21 10:27:37 +0200

    Correction de registered dans Feather.h et Sword.h

--- src/core/Weapons/Feather.cpp
+++ src/core/Weapons/Feather.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 using namespace std;
 
+bool Feather::registered = Factory::register_class_name(
+    "Feather", [](pugi::xml_node const& node) -> std::unique_ptr<Weapon> {
+    int power = node.attribute("effect").as_int();
+    std::string name = node.attribute("name").as_string();
+    int cost = node.attribute("cost").as_int();
+    return std::make_unique<Feather>(power, name, cost);
+    });
+
+
 Feather::Feather(int power, std::string name, int cost) : Weapon(power, name, cost, AttackType::ELOQUENCE), currentChoice(DebuffType::STRENGTH) {}
 
 void Feather::setDebuffChoice(DebuffType choice) { 
